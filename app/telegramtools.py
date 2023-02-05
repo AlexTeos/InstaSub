795cc92319683a6c5a86b936fe38fc526b05fae5
AlexTeos <alex.teos@yandex.ru>
2023-02-05 18:20:48 +0200

    Move to the latest p-t-b verion

--- app/telegramtools.py
+++ app/telegramtools.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import zipfile
 import logging
-from telegram import Update, Bot, InputMediaPhoto, InputMediaVideo
-from telegram.ext import (Updater, CommandHandler, CallbackContext, Filters,
+from telegram import Update, InputMediaPhoto, InputMediaVideo
+from telegram.ext import (Application, CommandHandler, ContextTypes, filters,
                           MessageHandler)
 from instagramtools import (PrivateAccountException, UserNotFound,
                             MediaNotFound, HighlightNotFound)
@@ -16,46 +16,42 @@ class TelegramTools:
         self.logger = logging.getLogger('instasub')
         self.ig_tools = ig_tools
         self.logger.info('Sing in to telegram bot: id - {0}'.format(bot_token))
-        self.bot = Bot(bot_token)
-        self.updater = Updater(bot_token)
-        self.dispatcher = self.updater.dispatcher
+        self.application = Application.builder().token(bot_token).build()
+        self.application.add_handler(CommandHandler('start', self.help_command))
+        self.application.add_handler(CommandHandler('help', self.help_command))
+        self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.resolve_command))
 
-        self.dispatcher.add_handler(CommandHandler('start', self.help_command))
-        self.dispatcher.add_handler(CommandHandler('help', self.help_command))
-        self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.resolve_command))
+        self.application.run_polling()
 
-        self.updater.start_polling()
-        self.updater.idle()
-
-    def help_command(self, update: Update, context: CallbackContext) -> None:
-        update.message.reply_text(
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text(
             'Send me an username and I will send you an archived profile! Also you can send me a link to a story, post or highlight!')
 
-    def resolve_command(self, update: Update, context: CallbackContext) -> None:
+    async def resolve_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         self.logger.info('New request from {0}: {1}'.format(update.message.from_user.id, update.message.text))
         if '/highlights/' in update.message.text:
-            return self.download_highlight(update, context)
+            return await self.download_highlight(update, context)
         elif '/stories/' in update.message.text:
-            return self.download_story(update, context)
+            return await self.download_story(update, context)
         elif '/p/' in update.message.text or '/reel/' in update.message.text:
-            return self.download_media(update, context)
+            return await self.download_media(update, context)
         else:
             try:
-                return self.download_profile(update, context)
+                return await self.download_profile(update, context)
             except UserNotFound:
                 self.logger.warning(
                     'Unrecognized request from {0}: {1}'.format(update.message.from_user.id, update.message.text))
                 reply_message = update.message.reply_text('I don\'t know what to do with that. Try something else')
 
-    def download_story(self, update: Update, context: CallbackContext) -> None:
+    async def download_story(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         try:
-            reply_message = update.message.reply_text('Downloading story...')
+            reply_message = await update.message.reply_text('Downloading story...')
             story_path = self.ig_tools.download_story_from_url(update.message.text)
-            reply_message.edit_text('Here is your story')
+            await reply_message.edit_text('Here is your story')
             if str(story_path).endswith('.mp4'):
-                update.message.reply_video(open(story_path, 'rb'))
+                await update.message.reply_video(open(story_path, 'rb'))
             else:
-                update.message.reply_photo(open(story_path, 'rb'))
+                await update.message.reply_photo(open(story_path, 'rb'))
 
             os.remove(story_path)
 
@@ -66,11 +62,11 @@ class TelegramTools:
             self.logger.debug(
                 'Story request from {0} was not completed - story not found: {1}'.format(update.message.from_user.id,
                                                                                          update.message.text))
-            reply_message.edit_text('Story not found')
+            await reply_message.edit_text('Story not found')
 
-    def download_media(self, update: Update, context: CallbackContext) -> None:
+    async def download_media(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         try:
-            reply_message = update.message.reply_text('Downloading media...')
+            reply_message = await update.message.reply_text('Downloading media...')
             media_paths = self.ig_tools.download_media_from_url(update.message.text)
             medias = []
             for media_path in media_paths:
@@ -78,14 +74,13 @@ class TelegramTools:
                     medias.append(InputMediaVideo(media=open(media_path, 'rb')))
                 else:
                     medias.append(InputMediaPhoto(media=open(media_path, 'rb')))
-            reply_message.edit_text('Here is your media')
+            await reply_message.edit_text('Here is your media')
             caption = self.ig_tools.get_media_info_from_url(update.message.text)
             if len(caption) > 1024:
-                update.message.reply_media_group(medias)
-                update.message.reply_text(caption)
+                await update.message.reply_media_group(medias)
+                await update.message.reply_text(caption)
             else:
-                medias[0].caption = caption
-                update.message.reply_media_group(medias)
+                await update.message.reply_media_group(media=medias, caption=caption)
 
             for media_path in media_paths:
                 os.remove(media_path)
@@ -97,13 +92,13 @@ class TelegramTools:
             self.logger.debug(
                 'Media request from {0} was not completed - media not found: {1}'.format(update.message.from_user.id,
                                                                                          update.message.text))
-            reply_message.edit_text('Media not found')
+            await reply_message.edit_text('Media not found')
 
-    def download_highlight(self, update: Update, context: CallbackContext) -> None:
+    async def download_highlight(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         try:
-            reply_message = update.message.reply_text('Downloading highlight...')
+            reply_message = await update.message.reply_text('Downloading highlight...')
             highlight_paths = self.ig_tools.download_highlights_from_url(update.message.text)
-            reply_message.edit_text('Here is your highlights')
+            await reply_message.edit_text('Here is your highlights')
             highlights = []
             highlight_counter = 0
             highlight_index = 0
@@ -117,7 +112,7 @@ class TelegramTools:
                 highlight_index = highlight_index + 1
 
                 if highlight_counter == 10 or highlight_index == len(highlight_paths):
-                    update.message.reply_media_group(highlights)
+                    await update.message.reply_media_group(highlights)
                     highlights = []
                     highlight_counter = 0
 
@@ -132,7 +127,7 @@ class TelegramTools:
                 'Highlight request from {0} was not completed - highlight not found: {1}'.format(
                     update.message.from_user.id,
                     update.message.text))
-            reply_message.edit_text('Highlight not found')
+            await reply_message.edit_text('Highlight not found')
 
     class SplitArchiver:
         file = None
@@ -183,91 +178,73 @@ class TelegramTools:
             info_file.write(str)
         return path
 
-    def download_profile(self, update: Update, context: CallbackContext) -> None:
+    async def gen_input(self, user_id):
+        # Media
+        medias = self.ig_tools.get_user_medias(user_id)
+        for media in medias:
+            download_path = user_id + '/media/' + media.taken_at.strftime("%d.%m.%y %H-%M-%S") + '/'
+            info_file = self.save_to_file(
+                self.ig_tools.get_media_info(media) + self.ig_tools.get_media_comments(media),
+                download_path + media.pk + '.txt')
+            yield info_file
+            files = self.ig_tools.download_media(media, download_path)
+            for file in files:
+                yield file
+
+        # self.logger.debug(
+        #    '{0}\'s medias downloaded successfully: {1}'.format(update.message.text, update.message.from_user.id))
+
+        # Tagged media
+        tagged_medias = self.ig_tools.get_user_tagged_medias(user_id)
+        for tagged_media in tagged_medias:
+            download_path = user_id + '/tagged_media/' + tagged_media.taken_at.strftime("%d.%m.%y %H-%M-%S") + '/'
+            info_file = self.save_to_file(
+                self.ig_tools.get_media_info(tagged_media) + self.ig_tools.get_media_comments(tagged_media),
+                download_path + tagged_media.pk + '.txt')
+            yield info_file
+            files = self.ig_tools.download_media(tagged_media, download_path)
+            for file in files:
+                yield file
+
+        # self.logger.debug(
+        #    '{0}\'s tagged medias downloaded successfully: {1}'.format(update.message.text,
+        #                                                               update.message.from_user.id))
+
+        # Highlight
+        highlights = self.ig_tools.get_highlights(user_id)
+        for highlight in highlights:
+            download_path = user_id + '/highlights/' + highlight.created_at.strftime(
+                "%d.%m.%y %H-%M-%S") + '/'
+            info_file = self.save_to_file(self.ig_tools.get_highlight_info(highlight),
+                                          download_path + highlight.pk + '.txt')
+            yield info_file
+            files = self.ig_tools.download_highlight(highlight, download_path)
+            for file in files:
+                yield file
+
+        # self.logger.debug(
+        #    '{0}\'s highlights downloaded successfully: {1}'.format(update.message.text,
+        #                                                            update.message.from_user.id))
+
+    async def download_profile(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         try:
             self.logger.debug(
                 'Starting to download account {0} requested by {1}'.format(update.message.text,
                                                                            update.message.from_user.id))
 
-            reply_message = update.message.reply_text('Checking user...')
+            reply_message = await update.message.reply_text('Checking user...')
             user_id = self.ig_tools.get_user_id(update.message.text)
             archiver = self.SplitArchiver(user_id, self.FILE_SIZE_LIMIT)
 
-            # Media
-            medias = self.ig_tools.get_user_medias(user_id)
-            media_counter = 0
-            for media in medias:
-                download_path = user_id + '/media/' + media.taken_at.strftime("%d.%m.%y %H-%M-%S") + '/'
-                info_file = self.save_to_file(
-                    self.ig_tools.get_media_info(media) + self.ig_tools.get_media_comments(media),
-                    download_path + media.pk + '.txt')
-                archiver.write(info_file, os.path.relpath(info_file, user_id))
-                files = self.ig_tools.download_media(media, download_path)
-                for file in files:
-                    archive = archiver.write(file, os.path.relpath(file, user_id))
-                    if archive:
-                        update.message.reply_document(open(archive, 'rb'))
-                        os.remove(archive)
-                reply_message.edit_text(
-                    '{0} posts out of {1} have been downloaded'.format(media_counter + 1, len(medias)))
-                media_counter = media_counter + 1
-
-            self.logger.debug(
-                '{0}\'s medias downloaded successfully: {1}'.format(update.message.text, update.message.from_user.id))
-
-            # Tagged media
-            tagged_medias = self.ig_tools.get_user_tagged_medias(user_id)
-            tagged_media_counter = 0
-            for tagged_media in tagged_medias:
-                download_path = user_id + '/tagged_media/' + tagged_media.taken_at.strftime("%d.%m.%y %H-%M-%S") + '/'
-                info_file = self.save_to_file(
-                    self.ig_tools.get_media_info(tagged_media) + self.ig_tools.get_media_comments(tagged_media),
-                    download_path + tagged_media.pk + '.txt')
-                archiver.write(info_file, os.path.relpath(info_file, user_id))
-
-                files = self.ig_tools.download_media(tagged_media, download_path)
-                for file in files:
-                    archive = archiver.write(file, os.path.relpath(file, user_id))
-                    if archive:
-                        update.message.reply_document(open(archive, 'rb'))
-                        os.remove(archive)
-                reply_message.edit_text(
-                    '{0} tagged posts out of {1} have been downloaded'.format(tagged_media_counter + 1,
-                                                                              len(tagged_medias)))
-                tagged_media_counter = tagged_media_counter + 1
-
-            self.logger.debug(
-                '{0}\'s tagged medias downloaded successfully: {1}'.format(update.message.text,
-                                                                           update.message.from_user.id))
-
-            # Highlight
-            highlights = self.ig_tools.get_highlights(user_id)
-            highlight_counter = 0
-            for highlight in highlights:
-                download_path = user_id + '/highlights/' + highlight.created_at.strftime(
-                    "%d.%m.%y %H-%M-%S") + '/'
-                info_file = self.save_to_file(self.ig_tools.get_highlight_info(highlight),
-                                              download_path + highlight.pk + '.txt')
-                archiver.write(info_file, os.path.relpath(info_file, user_id))
-
-                files = self.ig_tools.download_highlight(highlight, download_path)
-                for file in files:
-                    archive = archiver.write(file, os.path.relpath(file, user_id))
-                    if archive:
-                        update.message.reply_document(open(archive, 'rb'))
-                        os.remove(archive)
-
-            self.logger.debug(
-                '{0}\'s highlights downloaded successfully: {1}'.format(update.message.text,
-                                                                        update.message.from_user.id))
-
-            if media_counter + highlight_counter + tagged_media_counter == 0:
-                reply_message.edit_text('User don\'t have any media')
-                return None
+            async for file in self.gen_input(user_id):
+                archive = archiver.write(file, os.path.relpath(file, user_id))
+                if archive:
+                    await update.message.reply_document(open(archive, 'rb'))
+                    os.remove(archive)
 
             archive = archiver.close()
             if archive:
-                update.message.reply_document(open(archive, 'rb'))
+                await update.message.reply_document(open(archive, 'rb'))
                 os.remove(archive)
 
             if os.path.exists(user_id):
@@ -279,9 +256,9 @@ class TelegramTools:
         except PrivateAccountException:
             self.logger.debug('Requested account {0} is private, request from {1} '.format(update.message.text,
                                                                                            update.message.from_user.id))
-            reply_message.edit_text('The account is private')
+            await reply_message.edit_text('The account is private')
         except UserNotFound:
             self.logger.debug('Requested account {0} does not exits, request from {1} '.format(update.message.text,
                                                                                                update.message.from_user.id))
-            reply_message.edit_text('Invalid link or username')
+            await reply_message.edit_text('Invalid link or username')
             raise
